58e2a32e3afb8abb1380d8163ecd37bdb0bf714d
thisiszhou <thisiszhou@tencent.com>
2024-07-04 11:44:44 +0800

    change

--- update.bat
+++ update.bat
@@ -1,26 +1,20 @@
 @echo off
 REM 添加所有更改到Git
 git add .
-pause
 
 REM 提交更改
 git commit -m "change"
-pause
 
 REM 推送更改到远程仓库
 git push
-pause
 
 REM 发送POST请求到指定URL
 curl http://10.76.143.36:40041/actuator/busrefresh/vote-demo -X POST
-pause
 
-REM 检查curl命令的返回码
-if %errorlevel% neq 0 (
-    echo "curl命令失败"
-    exit /b %errorlevel%
-)
-pause
+@REM REM 检查curl命令的返回码
+@REM if %errorlevel% neq 0 (
+@REM     echo "curl命令失败"
+@REM     exit /b %errorlevel%
+@REM )
 
 echo "所有命令执行完毕"
-pause
