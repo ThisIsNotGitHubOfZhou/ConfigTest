8cceb59f350e265ce2a37bafa0779de1e04d537b
thisiszhou <thisiszhou@tencent.com>
2024-07-04 11:46:39 +0800

    change

--- update.bat
+++ update.bat
@@ -11,10 +11,10 @@ git push
 REM 发送POST请求到指定URL
 curl http://10.76.143.36:40041/actuator/busrefresh/vote-demo -X POST
 
-@REM REM 检查curl命令的返回码
-@REM if %errorlevel% neq 0 (
-@REM     echo "curl命令失败"
-@REM     exit /b %errorlevel%
-@REM )
+REM 检查curl命令的返回码
+if %errorlevel% neq 0 (
+    echo "curl命令失败"
+    exit /b %errorlevel%
+)
 
-echo "updatesuccess"
+echo "所有命令执行完毕"
